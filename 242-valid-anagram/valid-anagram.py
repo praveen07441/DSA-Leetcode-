9ddfefdcceb76e5e1bbe419bc280e8cd11f5b14d
Praveen.S <praveenshekar03@gmail.com>
2026-02-15 13:40:37 +0530

    Time: 24 ms (36.53%) | Memory: 12.5 MB (98.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        from collections import Counter
+        if Counter(s) == Counter(t):
+            return True
+        else:
+            return False
